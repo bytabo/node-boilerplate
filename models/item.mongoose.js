34891033899cc47f8cf207118732cc10382cf428
Johannes Höhn <johannes.hoehn@bytabo.de>
2021-04-09 09:45:29 +0200

    rename collection

--- models/item.mongoose.js
+++ models/item.mongoose.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
+const collectionName = `items_${process.env.NODE_ENV === 'production' ? 'live' : 'test'}`;
+
 const Item = mongoose.model('Item', {
     name: String,
     description: String,
     isChecked: Boolean,
     listIndex: Number,
     isImportant: Boolean,
-}, {
-    collection: `items_${process.env.NODE_ENV === 'production' ? 'live' : 'test'}`,
-});
+}, collectionName);
 
 module.exports = Item;
