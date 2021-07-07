721d2ab4f31ff71d1e52dfa108f916dbf31dc5e9
Tobias Weiss <tobias.weiss@bytabo.de>
2021-07-07 17:20:52 +0200

    outsourced swagger config to js

--- app.js
+++ app.js
@@ -20,89 +20,15 @@ app.use('/items', require('./routes/items'));
 app.use('/users', require('./routes/users'));
 app.use('/books', require('./routes/books'));
 
-const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require("swagger-ui-express");
-
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "LogRocket Express API with Swagger",
-            version: "0.1.0",
-            description:
-                "This is a simple CRUD API application made with Express and documented with Swagger",
-            license: {
-                name: "MIT",
-                url: "https://spdx.org/licenses/MIT.html",
-            },
-            contact: {
-                name: "LogRocket",
-                url: "https://logrocket.com",
-                email: "info@email.com",
-            },
-        },
-        servers: [
-            {
-                url: "http://localhost:3000/api/",
-            },
-            {
-                url: "https://develop.bytabo.de:3000/api/",
-            },
-        ],
-        tags: [
-            {
-                name: 'Pets'
-            }
-        ],
-        paths: {
-            "/pets": {
-                "get": {
-                    tags: ['Pets'],
-                    description: "Returns all pets from the system that the user has access to",
-                    operationId: 'getPets',
-                    security: [
-                        {
-                            bearerAuth: []
-                        }
-                    ],
-                    responses: {
-                        "200": {
-                            description: "A list of pets.",
-                            "content": {
-                                "application/json": {
-                                    schema: {
-                                        type: "array",
-                                        items: {
-                                            pet_name: {
-                                                type: 'string',
-                                                description: 'Pet Name'
-                                            },
-                                            pet_age: {
-                                                type: 'string',
-                                                description: 'Pet Age'
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
-    apis: ["./routes/books.js"]
-};
+const swaggerOptions = require("./swagger");
 
 
 
-
-
-const specs = swaggerJsdoc(options);
 app.use(
     "/api-docs",
     swaggerUi.serve,
-    swaggerUi.setup(specs)
+    swaggerUi.setup(swaggerOptions)
 );
 
 module.exports = app;
